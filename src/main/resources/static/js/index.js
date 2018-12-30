3e660aec3e4142f840fc1760d5389524336dbee3
enoch <531207583@qq.com>
2018-12-30 18:10:44 +0800

    dafadf

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -9,7 +9,7 @@ $.ajax({
             //alert(data.data.username);
         	var menus=data.data;
         	var menuUl=$("#menu");
-        	//menuUl.html("helll");
+        	menuUl.html("");
         	//第一层
         	for(var i in menus){
         		if(menus[i].menuLevel=='1'){
@@ -33,16 +33,3 @@ $.ajax({
         alert("服务器出错！");
     }
 });
-
-/*<li class="layui-nav-item layui-nav-itemed">
-<a class="layui-nav-item" href="javascript:;">项目</a>
-<dl class="layui-nav-child">
-  <dd><a href="javascript:;">项目列表</a></dd>
-  <dd><a href="javascript:;" onclick="changeMenu('/welcome')">我的项目</a></dd>
-</dl>
-</li>
-<li class="layui-nav-item"><a href="#">通知</a>
-	 <dl class="layui-nav-child">
-  	<dd><a href="javascript:;" onclick="changeMenu('/message')">我的消息</a></dd>
-	 </dl>
-</li>*/
