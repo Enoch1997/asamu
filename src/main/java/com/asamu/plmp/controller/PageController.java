1963b71577720270de00156b17ade0f3739cc32e
enoch <531207583@qq.com>
2018-12-30 13:16:08 +0800

    test

--- src/main/java/com/asamu/plmp/controller/PageController.java
+++ src/main/java/com/asamu/plmp/controller/PageController.java
@@ -1,15 +1,21 @@
 package com.asamu.plmp.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.asamu.plmp.dao.UserDAO;
+import com.asamu.plmp.pojo.entity.UserDO;
 import com.asamu.plmp.pojo.vo.JsonResult;
 
 @Controller
 @RequestMapping("/")
 public class PageController {
-
+	@Autowired
+	private UserDAO userDAO;
 	
 	@RequestMapping("index")
 	public String login() {
@@ -27,8 +33,11 @@ public class PageController {
 	}
 	
 	@RequestMapping("/getUserMenu")
-	public JsonResult getUserMenu() {
-		
-		return null;
+	@ResponseBody
+	public JsonResult getUserMenu(Integer userId) {
+		Optional<UserDO> op = userDAO.findById(userId);
+		UserDO userDO = op.get();
+		System.out.println(userDO.toString());
+		return JsonResult.success(userDO);
 	}
 }
