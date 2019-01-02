996a614486cc8fa3333db5401461a90930b41792
yuchenhuan <735405239@qq.com>
2019-01-02 19:46:05 +0800

    qawww

--- src/main/java/com/asamu/plmp/dao/AllocationDAO.java
+++ src/main/java/com/asamu/plmp/dao/AllocationDAO.java
@@ -1,9 +1,15 @@
 package com.asamu.plmp.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.asamu.plmp.pojo.entity.ExpertReview;
 
 public interface AllocationDAO extends JpaRepository<ExpertReview, Integer>{
 
+	
+	@Query("select u from ExpertReview u where u.expertId = ?1")
+	List<ExpertReview> findByExpertId(Integer expertId);
 }
